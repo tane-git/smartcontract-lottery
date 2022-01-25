9b87d5dc865da7b76b981f4725b5435c30820f17
tane-git <tanelives@gmail.com>
2022-01-25 19:46:58 +1300

    add sol contract with openzeppelin

---
+++ contracts/Lottery.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT  
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery is Ownable {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10 ** 18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEnranceFee(), 'Not enough ETH!');
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee () public view returns (uint256) {
+        (, int256 price, ,,) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            'Lottery can only start if it is closed'
+            );
+            lottery_state = LOTTERY_STATE.OPEN; 
+        }
+
+    function endLotter() public onlyOwner {}
+}
---
+++ tests/test_lottery.py
@@ -0,0 +1,12 @@
+from brownie import Lottery, accounts, config, network
+from web3 import Web3
+
+def test_get_entrance_fee():
+    account = accounts[0]
+    lottery = Lottery.deploy (
+        config["networks"][network.show_active()]['eth_usd_price_feed'],
+        {'from': account}
+    )
+    # these prices will change!
+    assert lottery.getEntranceFee() > Web3.toWei(0.01, 'ether')
+    assert lottery.getEntranceFee() < Web3.toWei(0.07, 'ether')
