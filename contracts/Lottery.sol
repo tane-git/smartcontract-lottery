a10c6eeadcdb86e1847b45b32b5246f0bfc9219e
tane-git <tanelives@gmail.com>
2022-01-26 11:36:16 +1300

    add Lottery.sol which compiles, at 7:07:50 in tutorial

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
@@ -15,17 +16,29 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
+    address payable public recentWinner;
+    uint256 public randomness;
 
-    constructor(address _priceFeedAddress) public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10 ** 18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
         // $50 minimum
         require(lottery_state == LOTTERY_STATE.OPEN);
-        require(msg.value >= getEnranceFee(), 'Not enough ETH!');
+        require(msg.value >= getEntranceFee(), 'Not enough ETH!');
         players.push(msg.sender);
     }
 
@@ -44,5 +57,24 @@ contract Lottery is Ownable {
             lottery_state = LOTTERY_STATE.OPEN; 
         }
 
-    function endLotter() public onlyOwner {}
+    function endLottery() public onlyOwner {
+        // need a good random number generator
+
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    // keyword override allows this function to override the previous declaration of this function (from VRFConsumerBase)
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, 'Lottery can only be calculated if it is in the CALCULATING_WINNER state');
+        require(_randomness > 0, 'randomness not found');
+
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    } 
 }
