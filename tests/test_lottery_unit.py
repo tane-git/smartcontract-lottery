10e848a6e5e18d14a6c8aada8443c4e2b2afd47e
tane-git <tanelives@gmail.com>
2022-02-01 13:50:42 +1300

    remove example code

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -91,27 +91,3 @@ def test_can_pick_winner_correctly():
     assert lottery.recentWinner() == account 
     assert lottery.balance() == 0
     assert account.balance() == starting_balance_of_account + balance_of_lottery
-
-# def test_can_pick_winner_correctly():
-#     # Arrange
-#     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-#         pytest.skip()
-#     lottery = deploy_lottery()
-#     account = get_account()
-#     lottery.startLottery({"from": account})
-#     lottery.enter({"from": account, "value": lottery.getEntranceFee()})
-#     lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
-#     lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
-#     fund_with_link(lottery)
-#     starting_balance_of_account = account.balance()
-#     balance_of_lottery = lottery.balance()
-#     transaction = lottery.endLottery({"from": account})
-#     request_id = transaction.events["RequestedRandomness"]["requestId"]
-#     STATIC_RNG = 777
-#     get_contract("vrf_coordinator").callBackWithRandomness(
-#         request_id, STATIC_RNG, lottery.address, {"from": account}
-#     )
-#     # 777 % 3 = 0
-#     assert lottery.recentWinner() == account
-#     assert lottery.balance() == 0
-#     assert account.balance() == starting_balance_of_account + balance_of_lottery
